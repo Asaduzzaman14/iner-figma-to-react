bc38e9aac19a1479bd4971be0107b467bc61866c
Md Asaduzaman <asadasad9282@gmail.com>
2023-04-12 21:39:28 +0600

    push

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -25,7 +25,7 @@ const Blog = () => {
                                     <span className='font-semibold'>By Admin</span>
                                 </div>
 
-                                <h2 className='pb-4 text-[28px] text-[#002550] font-semibold'>Application of Blockchain in real estate industry</h2>
+                                <h2 className='pb-4 text-lg  xl:text-[28px] text-[#002550] font-semibold'>Application of Blockchain in real estate industry</h2>
 
                                 <p className='cart-desc text-base text-[#1A1A1A] opacity-60 text-justify'>Blockchain technology has been considered as one of the most revolutionary technological advances in recent years</p>
                             </div>
@@ -37,14 +37,14 @@ const Blog = () => {
 
                         <div className='card w-[360px]  lg:w-[455px] p-4 lg:p-6'>
                             <div className='text-start'>
-                                <img className=' w-[407px]' src={blog1} alt="" />
+                                <img className=' w-[407px]' src={blog2} alt="" />
                                 <h2 className='pt-5 lg:pt-[30px] lg:pb-7 text-[16px] text-[#002550] '>Real Estate, Analysis</h2>
                                 <div className='flex justify-between text-[16px] text-[#1A1A1A] pb-5 lg:pb-[30px]'>
                                     <span className='font-semibold '>09 jun 2022</span>
                                     <span className='font-semibold'>By Admin</span>
                                 </div>
 
-                                <h2 className='pb-4 text-[28px] text-[#002550] font-semibold'>Real estate Purchase Challenges in Bengaluru</h2>
+                                <h2 className='pb-4 text-lg  xl:text-[28px] text-[#002550] font-semibold'>Real estate Purchase Challenges in Bengaluru</h2>
 
                                 <p className='cart-desc text-base text-[#1A1A1A] opacity-60 text-justify'>There are several problems that buyers may face during real estate purchase in Bengaluru. Some of the major problems  are as follows</p>
                             </div>
@@ -55,14 +55,14 @@ const Blog = () => {
 
                         <div className='card w-[360px]  lg:w-[455px] p-4 lg:p-6'>
                             <div className='text-start'>
-                                <img className=' w-[407px]' src={blog1} alt="" />
+                                <img className=' w-[407px]' src={blog3} alt="" />
                                 <h2 className='pt-5 lg:pt-[30px] lg:pb-7 text-[16px] text-[#002550] '>Real Estate, Analysis</h2>
                                 <div className='flex justify-between text-[16px] text-[#1A1A1A] pb-5 lg:pb-[30px]'>
                                     <span className='font-semibold '>09 jun 2022</span>
                                     <span className='font-semibold'>By Admin</span>
                                 </div>
 
-                                <h2 className='pb-4 text-[28px] text-[#002550] font-semibold'>Artificial Intelligence in Realty</h2>
+                                <h2 className='pb-4 text-lg  xl:text-[28px] text-[#002550] font-semibold'>Artificial Intelligence in Realty</h2>
 
                                 <p className='cart-desc text-base text-[#1A1A1A] opacity-60 text-justify'>The Indian real estate market has been on an upswing in recent years, with a growing number of developers and investors entering the space</p>
                             </div>
